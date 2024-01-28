ae02bf6c14873d5d413fc5dbbbc90db76d871d92
mangat <mangat.mangat@outlook.com>
2024-01-28 14:31:55 +0530

    feat: testing sync

--- terraform/1-argocd.tf
+++ terraform/1-argocd.tf
@@ -4,6 +4,6 @@ resource "helm_release" "argocd" {
   chart = "argo-cd"
   namespace = "argocd"
   create_namespace = true
-  version = "3.35.4"
+  version = "5.53.0"
   values = [file("values/argocd.yml")]
 }
